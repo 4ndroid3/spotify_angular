9bc27f7919579897dae485bcb987c00c62438060
4ndroid3 <andijaimo@gmail.com>
2021-10-17 21:20:00 +0000

    funciona GET del endpoint

--- spotify/src/app/home/home.component.ts
+++ spotify/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-  import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { PeopleSWService } from '../people-sw.service';
+import { People } from '../people.model';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,20 @@ import { PeopleSWService } from '../people-sw.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  persona: any;
+  persona: People[] = []; 
 
-  constructor(private peopleService: PeopleSWService) { }
+  constructor(private httpClient: HttpClient,) { }
 
   ngOnInit(): void {
+    this.getPeople();
   }
 
-
-
+  getPeople() {
+    this.httpClient.get<any>('https://swapi.dev/api/people/1/').subscribe(
+      response => {
+        console.log(response);
+        this.persona = response;
+      }
+    );
+  }
 }
